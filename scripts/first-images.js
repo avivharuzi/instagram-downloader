6d34c794afc992e4e9264ca9437446fbb87a00ba
avivharuzi <avivharuzi@gmail.com>
2018-12-01 18:43:02 +0200

    Reformat code

--- scripts/first-images.js
+++ scripts/first-images.js
@@ -1,23 +1,23 @@
 function getEdgesFromGraphql(graphql) {
-    return graphql.user.edge_owner_to_timeline_media.edges;
+	return graphql.user.edge_owner_to_timeline_media.edges;
 }
 
 function getImagesFromeEdges(edges) {
-    var images = [];
-
-    for (var i = 0; i < edges.length; i++) {
-        images.push(edges[i].node.display_url);
-    }
-
-    return images;
+	var images = [];
+	
+	for (var i = 0; i < edges.length; i++) {
+		images.push(edges[i].node.display_url);
+	}
+	
+	return images;
 }
 
 function getFirstImages() {
-    var graphql = _sharedData.entry_data.ProfilePage[0].graphql;
-    var edges = getEdgesFromGraphql(graphql);
-    return getImagesFromeEdges(edges);
+	var graphql = _sharedData.entry_data.ProfilePage[0].graphql;
+	var edges = getEdgesFromGraphql(graphql);
+	return getImagesFromeEdges(edges);
 }
 
 function main() {
-    return getFirstImages();
+	return getFirstImages();
 }
