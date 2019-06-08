c01fb43f07560030143b1e92f0ac3dcfee9cdbe8
Aviv Haruzi <avivharuzi@gmail.com>
2019-06-09 01:59:00 +0300

    Replace from phantomjs to puppeteer

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const phantom = require('phantom');
-const prompts = require('prompts');
 const fse = require('fs-extra');
 const imageDownloader = require('image-downloader');
+const path = require('path');
+const prompts = require('prompts');
+const puppeteer = require('puppeteer');
 
 const BASE_INSTAGRAM_URL = 'https://www.instagram.com/';
 const BASE_PUBLIC_FOLDER = './public/';
 
 function getFilenameFromUrl(url) {
-	const matches = url.match(/\/([^\/?#]+)[^\/]*$/);
+  const matches = url.match(/\/([^\/?#]+)[^\/]*$/);
+
 	if (matches.length > 1) {
 		return matches[1];
-	}
+  }
+
 	return false;
 }
 
@@ -19,67 +22,77 @@ function download(url, path) {
 		url: url,
 		dest: path
 	};
-	
+
 	return imageDownloader.image(options);
 }
 
 async function saveImages(urls, username) {
 	const basicFolder = `${BASE_PUBLIC_FOLDER}${username}/`;
 	const isBasicFolderExist = await fse.exists(basicFolder);
-	
+
 	try {
 		if (!isBasicFolderExist) {
 			await fse.mkdir(basicFolder);
-		}
-		
-		for (let url of urls) {
-			const path = `${basicFolder}${getFilenameFromUrl(url)}`;
-			
-			await download(url, path);
-		}
+    }
+
+    urls = urls.reverse();
+
+    console.log(`Starting downloading images into folder: ${basicFolder}`);
+
+    for (let [i, url] of urls.entries()) {
+      const filename = getFilenameFromUrl(url);
+      const fileExtension = filename.substr(filename.lastIndexOf('.'));
+      const counter = i + 1;
+
+      let finaleFilename = '';
+
+      if (counter < 10) {
+        finaleFilename = `0${counter}${fileExtension}`;
+      } else {
+        finaleFilename = `${counter}${fileExtension}`;
+      }
+
+      const path = `${basicFolder}${finaleFilename}`;
+
+      await download(url, path);
+
+      if (((counter % (Math.floor(urls.length * 0.1))) === 0) || counter === urls.length) {
+        console.log(`Downloaded ${counter} images...`);
+      }
+    }
 	} catch (error) {
 		console.log(`There was a problem while downloading the file: ${error}`);
-	}
+  }
+
+  console.log('Finished download images');
 }
 
-(async function () {
+(async () => {
 	const question = await prompts({
 		type: 'text',
 		name: 'username',
 		message: 'Please insert instagram username',
 		validate: value => !value ? `Instagram username is required` : true
 	});
-	
+
 	const username = question.username;
-	
-	const instance = await phantom.create();
-	const page = await instance.createPage();
-	
-	await page.on('onResourceRequested', (data) => {
-		console.info('Resource requested: ', data.url);
-	});
-	
-	await page.on('onConsoleMessage', (data) => {
-		console.log('Console message', data);
-	});
-	
-	await page.on('onCallback', (data) => {
-		console.log('Callback: ', data);
-	});
-	
-	const status = await page.open(`${BASE_INSTAGRAM_URL}${username}`);
-	
-	if (status === 'success') {
-		await page.injectJs('./scripts/first-images.js');
-		
-		const urls = await page.evaluate(function () {
-			return main();
-		});
-		
-		await saveImages(urls, username);
-	} else {
-		console.log(`Could not load the page properly, status: ${status}`);
-	}
-	
-	await instance.exit();
+
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+  await page.goto(`${BASE_INSTAGRAM_URL}${username}`, {
+    timeout: 0
+  });
+
+  await page.addScriptTag({
+    path: path.join(__dirname, 'scripts/all-images.js')
+  });
+
+  const images = await page.evaluate(() => allImages());
+
+  console.log(`Found ${images.length} images`);
+
+  await browser.close();
+
+  await saveImages(images, username);
 })();
